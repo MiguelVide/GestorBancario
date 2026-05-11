4507e5fcc52229e3324a49f3fb46111f39afbe01
MiguelVide <miguelvide9@gmail.com>
2026-05-11 09:37:22 +0100

    adicionado persisntecia

--- src/bancario.py
+++ src/bancario.py
@@ -1,9 +1,35 @@
+import json
+import os
+
 from utils import gerar_id_bancario, validar_data, validar_nif, validar_email
+
+FICHEIRO_BANCARIOS = "bancarios.json"
 
 bancarios = {}
 
+
+# ==========================
+# Persistência
+# ==========================
+def guardar_bancarios():
+    with open(FICHEIRO_BANCARIOS, "w", encoding="utf-8") as ficheiro:
+        json.dump(bancarios, ficheiro, indent=4, ensure_ascii=False)
+
+
+def carregar_bancarios():
+    global bancarios
+
+    if os.path.exists(FICHEIRO_BANCARIOS):
+        with open(FICHEIRO_BANCARIOS, "r", encoding="utf-8") as ficheiro:
+            bancarios = json.load(ficheiro)
+    else:
+        bancarios = {}
+
+
 # CREATE
 def criar_bancario(nome, nif, email, morada, data_nascimento):
+    carregar_bancarios()
+
     if not validar_data(data_nascimento):
         return 400, "Data inválida. Utilize formato YYYY-MM-DD."
     if not validar_nif(nif):
@@ -23,17 +49,22 @@ def criar_bancario(nome, nif, email, morada, data_nascimento):
     }
 
     bancarios[id_bancario] = bancario
+    guardar_bancarios()
     return 201, bancario
 
 
 # READ
 def listar_bancarios():
+    carregar_bancarios()
+
     if not bancarios:
         return 404, "Não existem bancários registados."
     return 200, bancarios
 
 
 def consultar_bancario(id_bancario):
+    carregar_bancarios()
+
     if id_bancario not in bancarios:
         return 404, "Bancário não encontrado."
     return 200, {id_bancario: bancarios[id_bancario]}
@@ -41,6 +72,8 @@ def consultar_bancario(id_bancario):
 
 # UPDATE
 def atualizar_bancario(id_bancario, nome=None, nif=None, email=None, morada=None, data_nascimento=None):
+    carregar_bancarios()
+
     if id_bancario not in bancarios:
         return 404, "Bancário não encontrado."
 
@@ -65,18 +98,23 @@ def atualizar_bancario(id_bancario, nome=None, nif=None, email=None, morada=None
     if morada:
         bancarios[id_bancario]["morada"] = morada
 
+    guardar_bancarios()
     return 200, bancarios[id_bancario]
 
 
 # DELETE
 def remover_bancario(id_bancario):
+    carregar_bancarios()
+
     if id_bancario not in bancarios:
         return 404, "Bancário não encontrado."
 
     del bancarios[id_bancario]
+    guardar_bancarios()
     return 200, f"Bancário {id_bancario} removido."
 
 
 # AUX
 def existe_bancario(id_bancario):
+    carregar_bancarios()
     return id_bancario in bancarios
